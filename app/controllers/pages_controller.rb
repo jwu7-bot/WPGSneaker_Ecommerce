302d3b640dc23ef86d7332db289d74b9a2d296c5
JiaHui Wu <jwu7@rrc.ca>
2024-11-14 18:55:33 -0600

    updated the pages controller to display the show controller of the page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+  def show
+    @page = Page.find_by(permalink: params[:permalink])
+
+    if @page.nil?
+      render file: Rails.root.join("public", "404.html"), status: :not_found
+    end
+  end
 end
