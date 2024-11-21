663a02e5cbcc0a51104e913bb99dc432530bafff
JiaHui Wu <jwu7@rrc.ca>
2024-11-20 23:56:45 -0600

    updated products controller to allow serach

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,16 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    @categories = Category.all
+    @query = params[:query]
+    @category_id = params[:category_id]
+
+    @products = Product.all
+    @products = @products.where("name ILIKE ? OR description ILIKE ?", "%#{@query}%", "%#{@query}%") if @query.present?
+    @products = @products.where(category_id: @category_id) if @category_id.present?
+
+    @products = @products.page(params[:page]).per(10)
+  end
 end
