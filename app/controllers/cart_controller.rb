099ca71b4f4256f4bb9c52218ee64114b3121c93
JiaHui Wu <jwu7@rrc.ca>
2024-11-21 23:33:17 -0600

    updated the cart controller to display the item name added to the cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,13 +1,14 @@
 class CartController < ApplicationController
+  before_action :get_product, only: %i[create destroy]
+
   def create
     # add an item to the cart
     # POST /cart the post data will be in params, so we can access
     # the product id via params[:id]
-    product_id = params[:id].to_i
 
-    unless session[:cart].include?(product_id)
-      session[:cart] << product_id
-      flash[:notice] = "Item added to cart"
+    unless session[:cart].include?(@product.id)
+      session[:cart] << @product.id
+      flash[:notice] = "#{@product.name} was added to cart!"
     end
 
     redirect_to root_path
@@ -17,13 +18,19 @@ class CartController < ApplicationController
     # remove an item from the cart
     # DELETE /cart/:id, so the id will come from the path
     # and is accessible via the params[:id]
-    product_id = params[:id].to_i
 
-    if session[:cart].include?(product_id)
-      session[:cart].delete(product_id)
-      flash[:notice] = "Item was removed from cart!"
+    if session[:cart].include?(@product.id)
+      session[:cart].delete(@product.id)
+      flash[:notice] = "#{@product.name} was removed from cart!"
     end
 
     redirect_to root_path
+  end
+
+  private
+
+  def get_product
+    product_id = params[:id].to_i
+    @product = Product.find(product_id)
   end
 end
