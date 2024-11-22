277aadc89baef2dec74f430020bb7cadc50c8168
JiaHui Wu <jwu7@rrc.ca>
2024-11-21 22:50:03 -0600

    added helper methods to application controller to get session count; removed from product controller; updated the application layout to display the session count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :initialize_session
+  before_action :increment_visit_count
+  helper_method :visit_count
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1
+  end
+
+  def visit_count
+    session[:visit_count]
+  end
 end
