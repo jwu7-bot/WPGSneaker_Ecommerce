0335bf1f2a2fdd70576cb29d97bfc48906aa36ad
JiaHui Wu <jwu7@rrc.ca>
2024-11-21 23:03:52 -0600

    display the added item

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,9 +1,14 @@
 class CartController < ApplicationController
   def create
     # add an item to the cart
+    # POST /cart the post data will be in params, so we can access
+    # the product id via params[:id]
+    logger.debug("Adding id #{params["id"]} to the cart")
   end
 
   def destroy
     # remove an item from the cart
+    # DELETE /cart/:id, so the id will come from the path
+    # and is accessible via the params[:id]
   end
 end
