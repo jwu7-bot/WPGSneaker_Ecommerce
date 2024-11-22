847ff9a1afb4ba6cb8c36ff7ee970e80cfbc794b
JiaHui Wu <jwu7@rrc.ca>
2024-11-21 22:53:30 -0600

    removed session from application layout; added initialize session for our cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,10 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :initialize_session
-  before_action :increment_visit_count
-  helper_method :visit_count
 
   private
 
   def initialize_session
-    session[:visit_count] ||= 0
-  end
-
-  def increment_visit_count
-    session[:visit_count] += 1
-  end
-
-  def visit_count
-    session[:visit_count]
+    session[:cart] ||= []
   end
 end
