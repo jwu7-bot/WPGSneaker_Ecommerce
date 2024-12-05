adbeb84e0b92def7f4cf4b1cfbfdfdd7c10cdfd1
JiaHui Wu <jwu7@rrc.ca>
2024-12-04 23:59:49 -0600

    updated the breadcrumbs to show properly

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -2,16 +2,25 @@ module BreadcrumbsHelper
   def breadcrumbs
     crumbs = []
 
-    if controller_name == "categories" && action_name == "show"
-      crumbs << link_to("Home", root_path)
-      crumbs << link_to("Categories", categories_path)
-      crumbs << @category.name
+    if controller_name == "categories"
+      if action_name == "index"
+        # Categories Index Page
+        crumbs << link_to("Home", root_path)
+        crumbs << "Categories"
+      elsif action_name == "show"
+        # Category Show Page
+        crumbs << link_to("Home", root_path)
+        crumbs << link_to("Categories", categories_path)
+        crumbs << @category.name
+      end
     elsif controller_name == "products" && action_name == "show"
+      # Product Show Page
       crumbs << link_to("Home", root_path)
       crumbs << link_to("Categories", categories_path)
       crumbs << link_to(@product.category.name, category_path(@product.category)) if @product.category
       crumbs << @product.name
     else
+      # Default (e.g., Home Page)
       crumbs << link_to("Home", root_path)
     end
 
