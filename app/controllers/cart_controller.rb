469bd946fc3167bfcda34f223e001d7907923d72
JiaHui Wu <jwu7@rrc.ca>
2024-12-05 00:21:03 -0600

    updated the cart controller to allow update quantity

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,5 @@
 class CartController < ApplicationController
-  before_action :get_product, only: %i[create destroy]
+  before_action :get_product, only: %i[create destroy update_quantity]
 
   def create
     # add an item to the cart
@@ -7,7 +7,7 @@ class CartController < ApplicationController
     # the product id via params[:id]
 
     unless session[:cart].include?(@product.id)
-      session[:cart] << @product.id
+      session[:cart] << { product_id: @product.id, quantity: 1 }
       flash[:notice] = "#{@product.name} was added to cart!"
     end
 
@@ -24,6 +24,18 @@ class CartController < ApplicationController
       flash[:notice] = "#{@product.name} was removed from cart!"
     end
 
+    redirect_to root_path
+  end
+
+  def update_quantity
+    # Update the quantity of an item in the cart
+    quantity = params[:quantity].to_i
+    item = session[:cart].find { |item| item[:product_id] == @product.id }
+
+    if item
+      item[:quantity] = quantity
+      flash[:notice] = "#{@product.name} quantity updated to #{quantity}."
+    end
     redirect_to root_path
   end
 
