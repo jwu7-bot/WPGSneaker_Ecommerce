6ba0c957a06c00d414567383107c560256341457
JiaHui Wu <jwu7@rrc.ca>
2024-12-06 13:01:19 -0600

    updated the application controller to initialize cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :initialize_session
-  helper_method :cart
+  before_action :set_render_Cart
 
-  private
-
-  def initialize_session
-    session[:cart] ||= []
+  def set_render_cart
+    @render_cart = true
   end
 
-  def cart
-    Product.find(session[:cart])
+  def initialize_cart
+    @cart ||= Cart.find_by(id: session[:cart_id])
+
+    if @cart.nil?
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
   end
 end
